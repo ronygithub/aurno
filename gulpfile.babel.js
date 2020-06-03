fc44ee49784285c3e5184fbd5781e85eee18f2ca
ronygithub <ar.rony87@gmail.com>
2020-06-03 21:24:11 +0600

    HTML Boilarplate added

---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+// import gulp from 'gulp';
+// import yargs from 'yargs';
+// import sass from 'gulp-sass';
+// import cleanCSS from 'gulp-clean-css';
+// import gulpif from 'gulp-if';
+// import sourcemaps from 'gulp-sourcemaps';
+// import prefix from 'gulp-autoprefixer';
+
+// const PRODUCTION = yargs.argv.prod;
+// const paths = {   
+//     styles: {
+//         src: ['assets/scss/app.scss'],
+//         dest: ['assets/css/'],
+//         autoprefixerOptions: {
+//             browsers: ['last 2 versions', '> 5%']
+//         }
+//     }
+// }
+
+// export const styles = () =>{
+//     return gulp.src(paths.styles.src) 
+//     .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(prefix(paths.styles.autoprefixerOptions.browsers))
+//     .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+//     .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+//     .pipe(gulp.dest(paths.styles.dest))
+//   }
+
+// export const watch = () => {
+//     gulp.watch('assets/scss/**/*.scss', styles);
+// }
+
